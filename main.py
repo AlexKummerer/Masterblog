f0c72c01a5e4559655014571575b08765593cd4f
alexkummerer <alexander@thomas-kummerer.de>
2024-08-13 20:46:53 +0200

    create index route and show data in html

--- main.py
+++ main.py
@@ -1,12 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+blog_posts = [
+    {
+        "id": 1,
+        "author": "John Doe",
+        "title": "First Post",
+        "content": "This is my first post.",
+    },
+    {
+        "id": 2,
+        "author": "Jane Doe",
+        "title": "Second Post",
+        "content": "This is another post.",
+    },
+    {
+        "id": 3,
+        "author": "John Doe",
+        "title": "Third Post",
+        "content": "This is yet another post.",
+    },
+    {
+        "id": 4,
+        "author": "Jane Doe",
+        "title": "Fourth Post",
+        "content": "This is the last post.",
+    },
+    {
+        "id": 5,
+        "author": "John Doe",
+        "title": "Fifth Post",
+        "content": "This is the fifth post.",
+    }
+]
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+@app.route("/")
+def index():
+    # add code here to fetch the job posts from a file
+    return render_template("index.html", posts=blog_posts)
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
